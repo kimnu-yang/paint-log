a7f9406fc95db49bb30ac384a794b92335af7a12
Kinuk97 <kiukangg@gmail.com>
2024-01-30 19:08:02 +0900

    Refactor: 코드 정렬

--- app/src/main/java/com/diary/paintlog/view/activities/MainActivity.kt
+++ app/src/main/java/com/diary/paintlog/view/activities/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.appcompat.widget.PopupMenu
