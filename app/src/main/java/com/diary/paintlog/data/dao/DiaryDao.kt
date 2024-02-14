dfd25ee6051eaa7d1585347bdc550f7fd0aeedbe
Kinuk97 <kiukangg@gmail.com>
2024-02-14 19:08:19 +0900

    Feat: 태그 별 검색 추가

--- app/src/main/java/com/diary/paintlog/data/dao/DiaryDao.kt
+++ app/src/main/java/com/diary/paintlog/data/dao/DiaryDao.kt
@@ -13,8 +13,12 @@ import com.diary.paintlog.data.entities.DiaryWithTagAndColor
 @Dao
 interface DiaryDao {
     @Transaction
-    @Query("SELECT * FROM diary")
-    fun getAllDiaryWithTagAndColor(): List<DiaryWithTagAndColor>
+    @Query("SELECT * FROM diary ORDER BY id DESC")
+    fun getAllDiaryWithTagAndColor(): MutableList<DiaryWithTagAndColor>
+
+    @Transaction
+    @Query("SELECT * FROM diary WHERE id IN (SELECT diary_id FROM diary_tag WHERE tag = :tag) ORDER BY id DESC")
+    fun getAllDiaryByTagWithTagAndColor(tag: String): MutableList<DiaryWithTagAndColor>
 
     @Query("SELECT * FROM diary WHERE date(registered_at) = :date AND is_temp = :temp ORDER BY id DESC LIMIT 1")
     fun getDiaryWithTagAndColorByDate(date: String, temp: String): DiaryWithTagAndColor?
