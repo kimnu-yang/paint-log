580ad8268f1f0708d0427ebf93166e0170677322
Devgimun <devgimun@naver.com>
2024-02-28 10:12:28 +0900

    Feat: Art Entity 변경

--- app/src/main/java/com/diary/paintlog/data/entities/Art.kt
+++ app/src/main/java/com/diary/paintlog/data/entities/Art.kt
@@ -11,12 +11,6 @@ data class Art (
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "artist") val artist: String,
     @ColumnInfo(name = "resource_id") val resourceId: String,
-    @ColumnInfo(name = "red_ratio") val redRatio: Double = 0.0,
-    @ColumnInfo(name = "orange_ratio") val orangeRatio: Double = 0.0,
-    @ColumnInfo(name = "yellow_ratio") val yellowRatio: Double = 0.0,
-    @ColumnInfo(name = "green_ratio") val greenRatio: Double = 0.0,
-    @ColumnInfo(name = "blue_ratio") val blueRatio: Double = 0.0,
-    @ColumnInfo(name = "navy_ratio") val navyRatio: Double = 0.0,
-    @ColumnInfo(name = "violet_ratio") val violetRatio: Double = 0.0,
-    @ColumnInfo(name = "registered_at") val registeredAt: LocalDateTime = LocalDateTime.now()
+    @ColumnInfo(name = "rgb") val rgb: String,
+    @ColumnInfo(name = "registered_at") val registeredAt: LocalDateTime? = LocalDateTime.now()
 )
