5d6f5332e02dae4241ba02177fcecd8afcb060c9
Devgimun <devgimun@naver.com>
2024-02-28 10:13:36 +0900

    Feat: MyArt Dao, ViewModel 에 Insert 추가

--- app/src/main/java/com/diary/paintlog/data/dao/MyArtDao.kt
+++ app/src/main/java/com/diary/paintlog/data/dao/MyArtDao.kt
@@ -1,8 +1,10 @@
 package com.diary.paintlog.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
+import com.diary.paintlog.data.entities.MyArt
 import com.diary.paintlog.data.entities.MyAryWithInfo
 
 @Dao
@@ -15,4 +17,7 @@ interface MyArtDao {
     @Transaction
     @Query("SELECT * FROM my_art WHERE date(base_date) BETWEEN :startDate AND :endDate ORDER BY ID DESC LIMIT 1")
     fun getMyArtByBaseDate(startDate: String, endDate: String): MyAryWithInfo?
+
+    @Insert
+    fun insertMyArt(myArt: MyArt)
 }
