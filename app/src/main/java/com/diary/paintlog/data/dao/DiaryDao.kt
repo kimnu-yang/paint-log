67ec38b8b5fc1a434ce10e98dcd57f90932a301d
Kinuk97 <kiukangg@gmail.com>
2024-03-04 17:48:16 +0900

    Fix: 삭제일자 검사 오류 제거

--- app/src/main/java/com/diary/paintlog/data/dao/DiaryDao.kt
+++ app/src/main/java/com/diary/paintlog/data/dao/DiaryDao.kt
@@ -16,10 +16,10 @@ import java.time.LocalDateTime
 @Dao
 interface DiaryDao {
     @Transaction
-    @Query("SELECT * FROM diary WHERE is_temp = 'N'")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND is_temp = 'N'")
     fun getAllDiaryWithTagAndColor(): List<DiaryWithTagAndColor>
 
-    @Query("SELECT * FROM diary WHERE is_temp = 'N' AND id IN (:diaryIds)")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND is_temp = 'N' AND id IN (:diaryIds)")
     fun getAllDiaryWithTagAndColor(diaryIds: List<Long>): List<DiaryWithTagAndColor>
 
     @Query(
@@ -39,28 +39,28 @@ interface DiaryDao {
     )
     fun getAllDiaryRegisteredAt(lastSyncTime: LocalDateTime): List<DiaryOnlyDate>
 
-    @Query("SELECT id FROM diary WHERE registered_at LIKE :date ORDER BY id DESC LIMIT 1")
+    @Query("SELECT id FROM diary WHERE deleted_at IS NULL AND registered_at LIKE :date ORDER BY id DESC LIMIT 1")
     fun getDiaryId(date: String): Long?
 
     @Transaction
-    @Query("SELECT * FROM diary WHERE is_temp = 'N' AND registered_at BETWEEN :startDate AND :endDate ORDER BY registered_at")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND is_temp = 'N' AND registered_at BETWEEN :startDate AND :endDate ORDER BY registered_at")
     fun getWeekDiaryByStartEndDate(startDate: String, endDate: String): List<DiaryWithTagAndColor>
 
     @Transaction
-    @Query("SELECT * FROM diary WHERE is_temp = 'N' AND registered_at LIKE :date")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND is_temp = 'N' AND registered_at LIKE :date")
     fun getMonthDiaryWithTagAndColor(date: String): List<DiaryWithTagAndColor>
 
-    @Query("SELECT weather, count(1) as count FROM diary WHERE is_temp = 'N' AND registered_at LIKE :yearMonth GROUP BY weather")
+    @Query("SELECT weather, count(1) as count FROM diary WHERE deleted_at IS NULL AND is_temp = 'N' AND registered_at LIKE :yearMonth GROUP BY weather")
     fun getWeatherMonthCount(yearMonth: String): List<DiaryWeatherCount>
 
-    @Query("SELECT * FROM diary WHERE id = :diaryId")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND id = :diaryId")
     fun getDiaryWithTagAndColorById(diaryId: Long): DiaryWithTagAndColor?
 
     @Transaction
-    @Query("SELECT * FROM diary WHERE id IN (SELECT diary_id FROM diary_tag WHERE tag = :tag) ORDER BY id DESC")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND id IN (SELECT diary_id FROM diary_tag WHERE tag = :tag) ORDER BY id DESC")
     fun getAllDiaryByTagWithTagAndColor(tag: String): List<DiaryWithTagAndColor>
 
-    @Query("SELECT * FROM diary WHERE date(registered_at) = :date AND is_temp = :temp ORDER BY id DESC LIMIT 1")
+    @Query("SELECT * FROM diary WHERE deleted_at IS NULL AND date(registered_at) = :date AND is_temp = :temp ORDER BY id DESC LIMIT 1")
     fun getDiaryWithTagAndColorByDate(date: String, temp: String): DiaryWithTagAndColor?
 
     @Insert
@@ -69,8 +69,8 @@ interface DiaryDao {
     @Update
     fun updateDiary(diary: Diary)
 
-    @Query("DELETE FROM diary WHERE id = :diaryId")
-    fun deleteDiary(diaryId: Long)
+    @Query("UPDATE diary SET deleted_at = :time WHERE id = :diaryId")
+    fun deleteDiary(diaryId: Long, time: LocalDateTime = LocalDateTime.now())
 
     @RawQuery
     fun executeRawQuery(query: SupportSQLiteQuery): List<Any>
